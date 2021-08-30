f20e269f7a282208daddeca311d8e0f1b0e3ad05
Sirenitix <suleevn@gmail.com>
2021-08-30 18:04:35 +0600

    some new changes

--- src/main/java/com/example/BookShop/controller/MainPageController.java
+++ src/main/java/com/example/BookShop/controller/MainPageController.java
@@ -1,18 +1,11 @@
 package com.example.BookShop.controller;
 
-import com.example.BookShop.entity.Author;
 import com.example.BookShop.service.GeneralService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.List;
-import java.util.Map;
 
 @Controller
 @RequestMapping("/")
@@ -31,9 +24,7 @@ public class MainPageController {
 
     @GetMapping
     public String mainPage(Model model) {
-        model.addAttribute("srchPlcholder","something");
         model.addAttribute("bookData", generalService.getAllBooks());
-        model.addAttribute("serverTime",new SimpleDateFormat("yyyy-MM-dd").format(new Date()));
         return "index";
     }
 
@@ -51,5 +42,26 @@ public class MainPageController {
         model.addAttribute("authorData", generalService.getAlphabetAndAuthors());
         return "authors";
     }
+
+    @GetMapping("/books/recent")
+    public String noveltyPage(Model model) {
+        return "recent";
+    }
+
+    @GetMapping("/books/popular")
+    public String popularBooksPage(Model model) {
+        return "popular";
+    }
+
+    @GetMapping("/signin")
+    public String signinPage(Model model) {
+        return "signin";
+    }
+
+    @GetMapping("/documents")
+    public String docPage(Model model) {
+        return "signin";
+    }
+
 
 }
